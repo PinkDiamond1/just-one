cac2cfe42ee68d889340c83856c2a09f14a81884
julienbrg <julien@strat.cc>
2023-02-18 09:29:55 +0100

    add burn

--- test/NFT.ts
+++ test/NFT.ts
@@ -10,7 +10,7 @@ describe("Just One", function () {
 
     const uri = "ipfs://bafkreih2ac5yabo2daerkw5w5wcwdc7rveqejf4l645hx2px26r5fxfnpe"
     const NFT = await ethers.getContractFactory("NFT");
-    const nft = await NFT.deploy(uri);
+    const nft = await NFT.deploy(deployer.address, uri, "NFT", "NFT");
 
     return { nft, deployer }
   }
@@ -18,7 +18,13 @@ describe("Just One", function () {
   describe("Mint", function () {
     it("Should mint 1 NFT", async function () {
       const { nft, deployer } = await loadFixture(deployContracts);
+      expect(await nft.ownerOf(1)).to.be.equal(deployer.address)
+    })
+    it("Should burn id #1", async function () {
+      const { nft, deployer } = await loadFixture(deployContracts);
       expect (await nft.ownerOf(1)).to.be.equal(deployer.address)
+      await nft.burn()
+      expect(nft.ownerOf(1)).to.be.reverted
     })
   })
 })
